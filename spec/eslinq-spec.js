90967a8917a4ed26cae84fcfab43870ba67c5c32
Botond Balazs <balazsbotond@gmail.com>
2015-09-20 13:52:30 +0200

    Test cases for 'all'

--- spec/eslinq-spec.js
+++ spec/eslinq-spec.js
@@ -182,4 +182,71 @@ describe("from", () => {
 		});
 		
 	});
+	
+	describe("all", () => {
+		
+		it("returns true when given an empty iterable and a constant true condition", () => {
+			const original = [],
+				expected = true,
+				actual = from(original).all(_ => true);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns true when given an empty iterable and a constant false condition", () => {
+			const original = [],
+				expected = true,
+				actual = from(original).all(_ => false);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns true when given a one-element iterable and a constant true condition", () => {
+			const original = [1],
+				expected = true,
+				actual = from(original).all(_ => true);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns false when given a one-element iterable and a constant false condition", () => {
+			const original = [1],
+				expected = false,
+				actual = from(original).all(_ => false);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns true if all elements match", () => {
+			const original = [1, 2, 3],
+				expected = true,
+				matches = n => n < 4,
+				actual = from(original).all(matches);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns false if no elements match", () => {
+			const original = [1, 2, 3],
+				expected = false,
+				matches = n => n >= 4,
+				actual = from(original).all(matches);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("returns false if one element doesn't match", () => {
+			const original = [1, 2, 3],
+				expected = false,
+				matches = n => n < 3,
+				actual = from(original).all(matches);
+			expect(actual).toEqual(expected);
+		});
+		
+		it("works if the condition returns non-boolean truthy values", () => {
+			const original = [1, '1'];
+			expect(from(original).all(identity)).toBe(true);
+		});
+		
+		it("works if the condition returns non-boolean falsy values", () => {
+			const cases = [0, undefined, NaN, ""];
+			for (let c of cases)
+				expect(from([c]).all(identity)).toBe(false);
+		});
+		
+	});
 });
