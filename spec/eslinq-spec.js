c9e62e23179943579c336dc6092dac3a5a1e4c86
Botond Balazs <balazsbotond@gmail.com>
2015-09-20 21:44:51 +0200

    Test cases for 'distinct'

--- spec/eslinq-spec.js
+++ spec/eslinq-spec.js
@@ -377,5 +377,40 @@ describe("from", () => {
 		});
 
 	});
+	
+	describe(".distinct", () => {
+		
+		it("returns the empty iterable if given the empty iterable", () => {
+			expect(from([]).distinct().toArray()).toEqual([]);
+		});
+		
+		it("returns the element if given a one-element iterable", () => {
+			expect(from([1]).distinct().toArray()).toEqual([1]);
+		});
+		
+		it("returns [1] if given [1, 1]", () => {
+			expect(from([1, 1]).distinct().toArray()).toEqual([1]);
+		});
+		
+		it("returns [1, 2] if given [1, 2]", () => {
+			expect(from([1, 2]).distinct().toArray()).toEqual([1, 2]);
+		});
+		
+		it("returns [1, 2] if given [1, 1, 2]", () => {
+			expect(from([1, 1, 2]).distinct().toArray()).toEqual([1, 2]);
+		});
+		
+		it("returns [1, 2] if given [1, 2, 2]", () => {
+			expect(from([1, 2, 2]).distinct().toArray()).toEqual([1, 2]);
+		});
+		
+		it("returns [3, 2, 1] if given [3, 2, 3, 2, 2, 1, 1, 1, 2]", () => {
+			const original = [3, 2, 3, 2, 2, 1, 1, 1, 2],
+				expected = [3, 2, 1],
+				actual = from(original).distinct().toArray();
+			expect(actual).toEqual(expected);
+		});
+		
+	});
 
 });
