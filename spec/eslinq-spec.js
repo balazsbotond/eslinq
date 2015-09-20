e00afa75d8bdcb852873ad7093017c856594925c
Botond Balazs <balazsbotond@gmail.com>
2015-09-20 13:30:24 +0200

    Test cases for 'select': undefined, null, false

--- spec/eslinq-spec.js
+++ spec/eslinq-spec.js
@@ -65,6 +65,25 @@ describe("from", () => {
 				actual = from(original).select(double).toArray();
 			expect(actual).toEqual(expected); 
 		});
+		
+		const verifyWithConstant = (constant) => {
+			const original = [1, 2],
+				expected = [constant, constant],
+				actual = from(original).select(_ => constant).toArray();
+			expect(actual).toEqual(expected);
+		}
+		
+		it("works as expected if the transform function returns undefined", () => {
+			verifyWithConstant(undefined);
+		});
+		
+		it("works as expected if the transform function returns null", () => {
+			verifyWithConstant(null);
+		});
+		
+		it("works as expected if the transform function returns false", () => {
+			verifyWithConstant(false);
+		});
 
 	});
 	
