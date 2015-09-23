adfc21e627206e66c35ae80a5a83ff1595e6db28
Botond Balazs <balazsbotond@gmail.com>
2015-09-23 21:55:40 +0200

    Write more tests for `first`

--- spec/first-spec.js
+++ spec/first-spec.js
@@ -2,6 +2,7 @@
 
 import from from "../src/eslinq";
 import { Sequence } from "../src/eslinq";
+import { even } from "./helpers";
 
 describe(".first", () => {
 
@@ -19,18 +20,30 @@ describe(".first", () => {
 		expect(from([2]).first()).toBe(2);
 	});
 	
+	it("returns the element if called on a single-element sequence with a matching condition", () => {
+		expect(from([2]).first(even)).toBe(2);
+	});
+	
 	it("throws an error if the single element of a sequence does not match", () => {
 		const bomb = () => from([1]).first(n => n === 2);
 		expect(bomb).toThrowError("No matching element found");
 	});
 	
-	it("returns the element if called without a condition", () => {
+	it("returns the first element if called without a condition", () => {
 		expect(from([3, 4]).first()).toBe(3);
 	});
 	
 	it("throws an error if no elements match", () => {
 		const bomb = () => from([3, 4]).first(n => n === 2);
 		expect(bomb).toThrowError("No matching element found");
+	});
+	
+	it("returns the matching element from a sequence with one matching element", () => {
+		expect(from([3, 4, 5]).first(even)).toBe(4);
+	});
+	
+	it("returns the first matching element from a sequence with multiple matching element", () => {
+		expect(from([3, 4, 5, 6, 7]).first(even)).toBe(4);
 	});
 
 });
